6803d7f789a61eb10d738d26cdf435eba76e4781
Conrad Großer <grosserconrad@gmail.com>
2018-11-08 15:05:47 +0100

    First Stable Release

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 #### TITLE
 ### META
 __title__ = "DepCraw"
-__author__ = "Conrad Großer"
-__version__ = "0.01"
-__branch__ = "DEVELOPMENT"
+__author__ = "Conrad Grosser"
+__version__ = "1"
+__branch__ = "STABLE"
 
 ### HELP
 ## Startup Parameters
@@ -14,10 +14,12 @@ __branch__ = "DEVELOPMENT"
 # PARAMETER - FUNCTION
 
 ## Functionality/ Usage
+# -> readme.md
 
 ## TODO
 
 ## Known Issues
+# - If a file imports only modules already considered in the requirements it will still show up in the final file
 
 ### IMPORTS
 # Importing sys for argument handling
@@ -26,7 +28,7 @@ import sys
 import time
 # Importing default module argparse for argument parsing
 import argparse
-
+# Importing os for file handling
 import os
 
 ### HELPER FUNCTIONS
@@ -41,11 +43,17 @@ def splitter(string):
 			word += string[i]
 	return split
 
+def deleteIfExist(fileName):
+	# Deletes a file with given path if it exists
+	if os.path.isfile(fileName):
+		os.remove(fileName)
+
 ### FUNCTIONS
-def writeDependencies(path, data):
+def writeDependencies(fileName, data):
 	print('Writing requirements.txt...')
+	deleteIfExist(fileName)
 	# Open the file location
-	file = open(path, 'w+')
+	file = open(fileName, 'w+')
 	file.write('# Automatically generated requirements/ dependencies\n')
 	# Write all the dependencies into the files
 	for i in range(len(data)):
@@ -55,22 +63,23 @@ def writeDependencies(path, data):
 	file.close()
 	return 0
 
-def readFile(path):
+def readFile(fileName):
 	dependencies = []
 	# Read the file and store it in a cache
-	file = open(path, 'r')
+	file = open(fileName, 'r')
 	code = file.readlines()
 	file.close()
 
 	# Search the cache for code lines that start with an import or from statement
 	for i in range(len(code)):
 		if code[i].startswith('import') or code[i].startswith('from'):
-			dependencies.append(code[i].split()[1].split('.')[0] + '\n')
+			newDependency = code[i].split()[1].split('.')[0]
+			dependencies.append(newDependency + '\n')
 
 	if len(dependencies) == 0:
-		return False
+		return ''
 	else:
-		message = '# From file ' + path + ' the following dependencies were added:\n'
+		message = '# From file ' + fileName + ' the following dependencies were initially added:\n'
 		return [message] + dependencies
 
 ### MAIN
@@ -83,10 +92,9 @@ def main(args):
 	for root, subdirs, files in os.walk(args.input_path):
 		for filename in files:
 			if filename.endswith('.py'):
-				if readFile(os.path.join(root, filename)):
-					requirements += readFile(os.path.join(root, filename))
+				requirements += readFile(os.path.join(root, filename))
 
-	writeDependencies((args.output_path + '/requirements.txt' if args.output_path else 'requirements.txt'), requirements)
+	writeDependencies((args.output_path + '/requirements.txt' if args.output_path else 'requirements.txt'), list(set(requirements)))
 
 	if args.timer: print('BENCHMARK: ' + __title__ + ' took ' + str((time.time() - startTime) * 1000) + ' seconds for executing.')
 	print('STATUS: ' + __title__ + ' ended.')
